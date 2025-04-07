8f1b9f2e3e0639c8c33da5d15395c2ac6013d498
CarmonaMedinaMaycon <20213tn004@utez.edu.mx>
2025-04-07 14:51:05 -0600

    [UÂPDATE] getfirst por get(0)

--- src/main/java/com/grupoeimsa/sigeim/models/licenses/controller/dto/ResponseLicenseDTO.java
+++ src/main/java/com/grupoeimsa/sigeim/models/licenses/controller/dto/ResponseLicenseDTO.java
@@ -192,7 +192,7 @@ public class ResponseLicenseDTO {
         this.personId = license.getPerson() != null ? license.getPerson().getPersonId() : null;
         this.responsiveLicenseId =
                 license.getResponsivesLicenses() != null && !license.getResponsivesLicenses().isEmpty()
-                        ? license.getResponsivesLicenses().getFirst().getResponsiveCellphoneId()
+                        ? license.getResponsivesLicenses().get(0).getResponsiveCellphoneId()
                         : null;
         this.fullName = license.getPerson().getName() + " " +
                 license.getPerson().getLastname() + " " +
