b6b769230320c51d0b33b7f2390d8aba31b9d3b6
DaddyDany <20213tn101@utez.edu.mx>
2025-04-11 13:36:29 -0600

    [PUSH] To develop

--- src/main/java/com/grupoeimsa/sigeim/models/licenses/model/BeanLicense.java
+++ src/main/java/com/grupoeimsa/sigeim/models/licenses/model/BeanLicense.java
@@ -44,7 +44,7 @@ public class BeanLicense {
     @Column(name = "comments_outlook", nullable = false)
     private String commentsOutlook;
 
-    @Column(name = "phoneNumber", nullable = false)
+    @Column(name = "phone_authenticator", nullable = false)
     private String authPhoneNumber;
 
     //change
