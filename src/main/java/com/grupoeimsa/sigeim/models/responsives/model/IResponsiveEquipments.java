a2569eee9146625c96ca78466c04ff924cf165d7
DaddyDany <20213tn101@utez.edu.mx>
2025-04-15 00:48:13 -0600

    update

--- src/main/java/com/grupoeimsa/sigeim/models/responsives/model/IResponsiveEquipments.java
+++ src/main/java/com/grupoeimsa/sigeim/models/responsives/model/IResponsiveEquipments.java
@@ -3,26 +3,23 @@ package com.grupoeimsa.sigeim.models.responsives.model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
-public interface IResponsiveEquipments extends JpaRepository<BeanResponsiveEquipaments, Long> {
+public interface IResponsiveEquipments extends JpaRepository<BeanResponsiveEquipaments, Long>, JpaSpecificationExecutor<BeanResponsiveEquipaments> {
 
-    // IResponsiveEquipments.java
     @Query("""
-    SELECT r FROM BeanResponsiveEquipaments r
-    JOIN r.computerEquipament e
-    JOIN e.person p
-    WHERE (:estado IS NULL OR r.status = :estado)
-      AND (:search IS NULL OR LOWER(CONCAT(p.name, ' ', p.lastname, ' ', p.surname)) LIKE LOWER(CONCAT('%', :search, '%')))
-    ORDER BY
-      CASE WHEN :sort = 'asc' THEN r.creationDate END ASC,
-      CASE WHEN :sort = 'desc' THEN r.creationDate END DESC
+SELECT r FROM BeanResponsiveEquipaments r
+JOIN r.computerEquipament e
+JOIN e.person p
+WHERE (:estado IS NULL OR r.status = :estado)
+  AND (:search IS NULL OR LOWER(CONCAT(p.name, ' ', p.lastname, ' ', p.surname)) LIKE LOWER(CONCAT('%', :search, '%')))
 """)
     Page<BeanResponsiveEquipaments> searchResponsives(
             @Param("search") String search,
             @Param("estado") EStatus estado,
-            @Param("sort") String sort,
             Pageable pageable
     );
+
 }
--- src/main/java/com/grupoeimsa/sigeim/models/responsives/model/IResponsiveLicenses.java
+++ src/main/java/com/grupoeimsa/sigeim/models/responsives/model/IResponsiveLicenses.java
@@ -3,12 +3,13 @@ package com.grupoeimsa.sigeim.models.responsives.model;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 
-public interface IResponsiveLicenses extends JpaRepository<BeanResponsiveLicenses, Long> {
+public interface IResponsiveLicenses extends JpaRepository<BeanResponsiveLicenses, Long>, JpaSpecificationExecutor<BeanResponsiveLicenses> {
     List<BeanResponsiveLicenses> findByLicense_LicensesId(Long licensesId);
 
     @Query("""
