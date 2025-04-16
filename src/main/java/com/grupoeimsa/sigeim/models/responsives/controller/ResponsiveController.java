fa7c130bca4fefc53e98ab3cce79eb78716edd93
DaddyDany <20213tn101@utez.edu.mx>
2025-04-16 00:10:58 -0600

    [UPDATE] RESPONSIVE EQUIPMENT ENDPOINT

--- src/main/java/com/grupoeimsa/sigeim/models/responsives/controller/ResponsiveController.java
+++ src/main/java/com/grupoeimsa/sigeim/models/responsives/controller/ResponsiveController.java
@@ -26,6 +26,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -79,6 +80,7 @@ public class ResponsiveController {
     }
 
     @PostMapping("/equipments")
+    @PreAuthorize("hasRole('ADMIN')")
     public ResponseEntity<Page<ResponseResponsiveEquipmentsDto>> getResponsivesEquipments(
             @RequestBody RequestSearchResponsiveEquipmentsDto dto) {
         Page<ResponseResponsiveEquipmentsDto> result = responsiveService.getResponsivesEquipments(dto);
